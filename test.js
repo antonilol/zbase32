6552e8a492d801758e75d0325817763d0b03ae15
Antoni Spaanderman <56turtle56@gmail.com>
2023-02-05 00:48:53 +0100

    format

--- test.js
+++ test.js
@@ -6,18 +6,18 @@ const { randomBytes } = require('crypto');
 assert(zbase32.encode(Buffer.from('')) === '');
 assert(zbase32.encode(Buffer.from('hello')) === 'pb1sa5dx');
 assert(zbase32.encode(Buffer.from('string')) === 'qp48r4mqch');
-assert(zbase32.encode(Buffer.from([0,1,2,3,4,5,6,7,8])) === 'yyyoryarywdyqny');
+assert(zbase32.encode(Buffer.from([ 0, 1, 2, 3, 4, 5, 6, 7, 8 ])) === 'yyyoryarywdyqny');
 
 // decoding
 assert(zbase32.decode('').equals(Buffer.from('')));
 assert(zbase32.decode('pb1sa5dx').equals(Buffer.from('hello')));
 assert(zbase32.decode('qp48r4mqch').equals(Buffer.from('string')));
-assert(zbase32.decode('yyyoryarywdyqny').equals(Buffer.from([0,1,2,3,4,5,6,7,8])));
+assert(zbase32.decode('yyyoryarywdyqny').equals(Buffer.from([ 0, 1, 2, 3, 4, 5, 6, 7, 8 ])));
 
 // specific length random bytes
 for (let i = 0; i < 1000; i++) {
-    const bytes = randomBytes(i);
-    assert(zbase32.decode(zbase32.encode(bytes)).equals(bytes));
+	const bytes = randomBytes(i);
+	assert(zbase32.decode(zbase32.encode(bytes)).equals(bytes));
 }
 
 console.log('No test failed');
